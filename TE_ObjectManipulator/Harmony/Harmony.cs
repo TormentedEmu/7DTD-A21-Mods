2c66fbbccf9bc386d5e2709617c536c35210b659
V <TormentedEmu@users.noreply.github.com>
2024-03-27 19:12:41 +1100

    Updated Object manipulator for Alpha 21
    Included the source and compiled dll.  Works on Alpha 21.2 b37

---
+++ TE_ObjectManipulator/Harmony/Harmony.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using System.Reflection;
+
+namespace TormentedEmu_Mods_A19
+{
+  public class Harmony_TE_ObjectManipulator : IModApi
+  {
+    public void InitMod(Mod mod)
+    {
+      var harmony = new Harmony("TormentedEmu.Mods.A21");
+      harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+
+    [HarmonyPatch(typeof(GameManager), "SetCursorEnabledOverride")]
+    public class GameManager_SetCursorEnabledOverride
+    {
+      static bool Prefix(GameManager __instance, ref bool ___bCursorVisibleOverrideState)
+      {
+        if (!___bCursorVisibleOverrideState)
+        {
+          ___bCursorVisibleOverrideState = true;
+        }
+
+        return true;
+      }
+    }
+  }
+
+}
